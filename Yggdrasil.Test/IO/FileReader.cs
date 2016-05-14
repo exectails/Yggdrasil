9241d43439cda28400c9378658f0d6cb4f5c08d2
exec <exec@mabinoger.de>
2016-05-15 00:11:29 +0200

    Minor change

--- Yggdrasil.Test/IO/FileReader.cs
+++ Yggdrasil.Test/IO/FileReader.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Aura development team - Licensed under GNU GPL
 // For more information, see license file in the main folder
 
-using System;
 using System.Collections.Generic;
 using System.IO;
 using Xunit;

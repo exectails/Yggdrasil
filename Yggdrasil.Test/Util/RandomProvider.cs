8580bd6d76564a0afe6b48bf259dd99aaf5a5cb0
exec <exec@mabinoger.de>
2016-05-17 12:56:15 +0200

    Added unit test for RandomProvider

---
+++ Yggdrasil.Test/Util/RandomProvider.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see license file in the main folder
+
+using System;
+using System.Threading;
+using Xunit;
+using Yggdrasil.Util;
+
+namespace Yggdrasil.Test.Util
+{
+	public class RandomProviderTests
+	{
+		/// <summary>
+		/// Tests that the same thread gets the same Random object on
+		/// repeating calls to Get, and that two threads don't get the same
+		/// object.
+		/// </summary>
+		[Fact]
+		public void Get()
+		{
+			Random rnd1 = null, rnd2 = null;
+			Random rnd3 = null, rnd4 = null;
+
+			var thread1 = new Thread(() =>
+			{
+				rnd1 = RandomProvider.Get();
+				rnd3 = RandomProvider.Get();
+			});
+
+			var thread2 = new Thread(() =>
+			{
+				rnd2 = RandomProvider.Get();
+				rnd4 = RandomProvider.Get();
+			});
+
+			thread1.Start();
+			thread2.Start();
+
+			thread1.Join();
+			thread2.Join();
+
+			Assert.NotEqual(rnd1, rnd2);
+			Assert.Equal(rnd1, rnd3);
+			Assert.Equal(rnd2, rnd4);
+		}
+	}
+}

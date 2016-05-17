d99b453e0e695d8ddb8dff5570522919e1ce65aa
exec <exec@mabinoger.de>
2016-05-17 13:14:17 +0200

    Added unit tests for Extensions

---
+++ Yggdrasil.Test/Extensions/Random.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see license file in the main folder
+
+using System.Collections.Generic;
+using Xunit;
+using Yggdrasil.Extensions;
+using Yggdrasil.Util;
+
+namespace Yggdrasil.Test.Extensions
+{
+	public class RandomExtensionTests
+	{
+		[Fact]
+		public void RandomElementFromIList()
+		{
+			var list = new List<int>() { 11, 23, 34, 42 };
+
+			for (int i = 0; i < 1000000; ++i)
+				Assert.Contains(list.Random(), list);
+		}
+
+		[Fact]
+		public void Between()
+		{
+			var rnd = RandomProvider.Get();
+
+			for (int i = 0; i < 1000000; ++i)
+				Assert.InRange(rnd.Between(10, 20), 10, 20);
+		}
+
+		[Fact]
+		public void Rnd()
+		{
+			var list = new int[] { 101, 203, 305, 407 };
+			var rnd = RandomProvider.Get();
+
+			for (int i = 0; i < 1000000; ++i)
+				Assert.Contains(rnd.Rnd(list), list);
+		}
+	}
+}
---
+++ Yggdrasil.Test/Extensions/String.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see license file in the main folder
+
+using Xunit;
+using Yggdrasil.Extensions;
+
+namespace Yggdrasil.Test.Extensions
+{
+	public class StringExtensionTests
+	{
+		[Fact]
+		public void LevenshteinDistance()
+		{
+			Assert.Equal(0, "test1".LevenshteinDistance("test1"));
+			Assert.Equal(1, "test2".LevenshteinDistance("test3"));
+			Assert.Equal(4, "test4".LevenshteinDistance("test4 asd"));
+		}
+	}
+}

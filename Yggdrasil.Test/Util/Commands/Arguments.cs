543f52c3559041ac4660e681e7a760cb0639801c
exec <exec@mabinoger.de>
2016-06-30 22:42:27 +0200

    Added Arguments class
    This will replace the args array in CommandManager.

---
+++ Yggdrasil.Test/Util/Commands/Arguments.cs
@@ -0,0 +1,103 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see license file in the main folder
+
+using System;
+using Xunit;
+using Yggdrasil.Util.Commands;
+
+namespace Yggdrasil.Test.Util.Commands
+{
+	public class ArgumentsTest
+	{
+		[Fact]
+		public void ParseIndexed()
+		{
+			var args1 = new Arguments("test1 lorem ipsum dolor");
+			Assert.Equal(new string[] { "test1", "lorem", "ipsum", "dolor" }, args1.GetAll());
+
+			var args2 = new Arguments("test1 lorem ipsum \"dolor sit amet\"");
+			Assert.Equal(new string[] { "test1", "lorem", "ipsum", "dolor sit amet" }, args2.GetAll());
+
+			var args3 = new Arguments("  test1 lorem    ipsum             dolor");
+			Assert.Equal(new string[] { "test1", "lorem", "ipsum", "dolor" }, args3.GetAll());
+
+			var args4 = new Arguments("  test1 lorem    ipsum      \"       dolor\"");
+			Assert.Equal(new string[] { "test1", "lorem", "ipsum", "dolor" }, args4.GetAll());
+		}
+
+		[Fact]
+		public void ParseMixed()
+		{
+			var args1 = new Arguments(">item 1234 c1:0 c2:white c3:0xff00ff prefix:5678");
+
+			Assert.Equal(6, args1.Count);
+			Assert.Equal(new string[] { ">item", "1234", "c1:0", "c2:white", "c3:0xff00ff", "prefix:5678" }, args1.GetAll());
+
+			Assert.Equal(">item", args1.Get(0));
+			Assert.Equal("1234", args1.Get(1));
+			Assert.Throws<ArgumentException>(() => args1.Get(2));
+			Assert.Throws<ArgumentException>(() => args1.Get(-1));
+
+			Assert.Equal("0", args1.Get("c1"));
+			Assert.Equal("white", args1.Get("c2"));
+			Assert.Equal("0xff00ff", args1.Get("c3"));
+			Assert.Equal("5678", args1.Get("prefix"));
+			Assert.Equal("5678", args1.Get("prefix"));
+			Assert.Throws<ArgumentException>(() => args1.Get("foobar"));
+		}
+
+		[Fact]
+		public void ParseNamed()
+		{
+			// Quoted value
+			var args1 = new Arguments(">broadcast text:\"this is a test\"");
+
+			Assert.Equal(2, args1.Count);
+			Assert.Equal(new string[] { ">broadcast", "\"text:this is a test\"" }, args1.GetAll());
+
+			Assert.Equal(">broadcast", args1.Get(0));
+			Assert.Throws<ArgumentException>(() => args1.Get(1));
+
+			Assert.Equal("this is a test", args1.Get("text"));
+			Assert.Throws<ArgumentException>(() => args1.Get("foobar"));
+
+			// Quoted key and value
+			var args2 = new Arguments(">bc1 \"test text\":\"this is a test\"");
+
+			Assert.Equal(2, args2.Count);
+			Assert.Equal(new string[] { ">bc1", "\"test text:this is a test\"" }, args2.GetAll());
+
+			Assert.Equal(">bc1", args2.Get(0));
+			Assert.Throws<ArgumentException>(() => args2.Get(1));
+
+			Assert.Equal("this is a test", args2.Get("test text"));
+			Assert.Throws<ArgumentException>(() => args2.Get("foobar"));
+
+			// Quoted key with spaces
+			var args3 = new Arguments(">bc2 \"test text 2\":\"this is a test\" color:0x00ff12");
+
+			Assert.Equal(3, args3.Count);
+			Assert.Equal(new string[] { ">bc2", "\"test text 2:this is a test\"", "color:0x00ff12" }, args3.GetAll());
+
+			Assert.Equal(">bc2", args3.Get(0));
+			Assert.Throws<ArgumentException>(() => args3.Get(1));
+
+			Assert.Equal("this is a test", args3.Get("test text 2"));
+			Assert.Equal("0x00ff12", args3.Get("color"));
+			Assert.Throws<ArgumentException>(() => args3.Get("foobar"));
+
+			// Quoted key and value in one with spaces and whitespace padding
+			var args4 = new Arguments(">bc3 \"    test text 3  :  this is a test \" color:0x00ff13");
+
+			Assert.Equal(3, args4.Count);
+			Assert.Equal(new string[] { ">bc3", "\"test text 3:this is a test\"", "color:0x00ff13" }, args4.GetAll());
+
+			Assert.Equal(">bc3", args4.Get(0));
+			Assert.Throws<ArgumentException>(() => args4.Get(1));
+
+			Assert.Equal("this is a test", args4.Get("test text 3"));
+			Assert.Equal("0x00ff13", args4.Get("color"));
+			Assert.Throws<ArgumentException>(() => args4.Get("foobar"));
+		}
+	}
+}
---
+++ Yggdrasil/Util/Commands/Arguments.cs
@@ -0,0 +1,149 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see license file in the main folder
+
+using System;
+using System.Collections.Generic;
+
+namespace Yggdrasil.Util.Commands
+{
+	/// <summary>
+	/// Represents arguments in a command.
+	/// </summary>
+	public class Arguments
+	{
+		private List<string> _indexed;
+		private Dictionary<string, string> _named;
+
+		/// <summary>
+		/// Returns the amount of all parameters.
+		/// </summary>
+		public int Count { get { return _indexed.Count + _named.Count; } }
+
+		/// <summary>
+		/// Creates new Arguments instance.
+		/// </summary>
+		public Arguments()
+		{
+			_indexed = new List<string>();
+			_named = new Dictionary<string, string>();
+		}
+
+		/// <summary>
+		/// Creates new Arguments instance and parses the given line.
+		/// </summary>
+		public Arguments(string line)
+			: this()
+		{
+			this.Parse(line);
+		}
+
+		/// <summary>
+		/// Returns true if an argument with the given index exists.
+		/// </summary>
+		/// <param name="index"></param>
+		/// <returns></returns>
+		public bool Contains(int index)
+		{
+			return (index >= 0 && index < _indexed.Count);
+		}
+
+		/// <summary>
+		/// Returns true if an argument with the given name exists.
+		/// </summary>
+		/// <param name="index"></param>
+		/// <returns></returns>
+		public bool Contains(string name)
+		{
+			return _named.ContainsKey(name);
+		}
+
+		/// <summary>
+		/// Parses given line into this Arguments instance.
+		/// </summary>
+		/// <param name="line"></param>
+		public void Parse(string line)
+		{
+			var quote = false;
+
+			for (int i = 0, n = 0; i <= line.Length; ++i)
+			{
+				if ((i == line.Length || line[i] == ' ') && !quote)
+				{
+					if (i - n > 0)
+					{
+						var str = line.Substring(n, i - n);
+						var index = str.IndexOf(":");
+						if (index != -1)
+						{
+							var key = str.Substring(0, index).Trim(' ', '"');
+							var val = str.Substring(index + 1, str.Length - index - 1).Trim(' ', '"');
+
+							_named.Add(key, val);
+						}
+						else
+							_indexed.Add(str.Trim(' ', '"'));
+					}
+
+					n = i + 1;
+					continue;
+				}
+
+				if (line[i] == '"')
+					quote = !quote;
+			}
+		}
+
+		/// <summary>
+		/// Returns the argument at the given index.
+		/// </summary>
+		/// <param name="index"></param>
+		/// <returns></returns>
+		/// <exception cref="ArgumentException">
+		/// Thrown if index doesn't exist.
+		/// </exception>
+		public string Get(int index)
+		{
+			if (!this.Contains(index))
+				throw new ArgumentException("Index '" + index + "' doesn't exist.");
+
+			return _indexed[index];
+		}
+
+		/// <summary>
+		/// Returns the argument with the given name.
+		/// </summary>
+		/// <param name="name"></param>
+		/// <returns></returns>
+		/// <exception cref="ArgumentException">
+		/// Thrown if no argument with the given name exists.
+		/// </exception>
+		public string Get(string name)
+		{
+			if (!this.Contains(name))
+				throw new ArgumentException("Argument with name '" + name + "' doesn't exist.");
+
+			return _named[name];
+		}
+
+		/// <summary>
+		/// Returns all arguments, starting with the indexed ones in order,
+		/// followed by the named ones, with no order guaranteed.
+		/// </summary>
+		/// <returns></returns>
+		public List<string> GetAll()
+		{
+			var result = new List<string>();
+
+			result.AddRange(_indexed);
+			foreach (var kv in _named)
+			{
+				if (kv.Key.Contains(" ") || kv.Value.Contains(" "))
+					result.Add('"' + kv.Key + ":" + kv.Value + '"');
+				else
+					result.Add(kv.Key + ":" + kv.Value);
+			}
+
+			return result;
+		}
+	}
+}

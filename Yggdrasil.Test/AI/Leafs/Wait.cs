afc97686bd7f9d4a48b8935482c4ada5598e2e4c
exec <exec@mabinoger.de>
2016-12-09 17:53:06 +0100

    Added unit test for Wait

---
+++ Yggdrasil.Test/AI/Leafs/Wait.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see license file in the main folder
+
+using System;
+using System.Threading;
+using Xunit;
+using Yggrasil.Ai;
+using Yggrasil.Ai.Composites;
+using Yggrasil.Ai.Leafs;
+
+namespace Yggdrasil.Test.AI.Leafs
+{
+	public class WaitTests
+	{
+		[Fact]
+		public void Wait()
+		{
+			var state = new State();
+			var waitTime = 2000;
+
+			var sequence = new Sequence(
+				new Wait(TimeSpan.FromMilliseconds(waitTime))
+			);
+
+			Assert.Equal(RoutineStatus.Running, sequence.Act(state));
+			Thread.Sleep(1000);
+			Assert.Equal(RoutineStatus.Running, sequence.Act(state));
+			Thread.Sleep(1100);
+			Assert.Equal(RoutineStatus.Success, sequence.Act(state));
+		}
+	}
+}

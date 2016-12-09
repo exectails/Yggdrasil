46cea89a2f20aef3565633a606210b52f501122f
exec <exec@mabinoger.de>
2016-12-09 18:06:09 +0100

    Added unit tests for Execute and Print

---
+++ Yggdrasil.Test/AI/Leafs/Execute.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see license file in the main folder
+
+using Xunit;
+using Yggrasil.Ai;
+using Yggrasil.Ai.Composites;
+using Yggrasil.Ai.Leafs;
+
+namespace Yggdrasil.Test.AI.Leafs
+{
+	public class ExecuteTests
+	{
+		[Fact]
+		public void Execute()
+		{
+			var state = new State();
+			var test = 0;
+
+			var sequence = new Sequence(
+				new Execute(_ => test++)
+			);
+
+			for (int i = 1; i <= 100; ++i)
+			{
+				Assert.Equal(RoutineStatus.Success, sequence.Act(state));
+				Assert.Equal(i, test);
+				state.Reset();
+			}
+		}
+	}
+}
---
+++ Yggdrasil.Test/AI/Leafs/Print.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see license file in the main folder
+
+using System;
+using System.IO;
+using System.Text;
+using Xunit;
+using Yggrasil.Ai;
+using Yggrasil.Ai.Composites;
+using Yggrasil.Ai.Leafs;
+
+namespace Yggdrasil.Test.AI.Leafs
+{
+	public class PrintTests
+	{
+		[Fact]
+		public void Print()
+		{
+			var state = new State();
+			var test = "foobar";
+
+			var sequence = new Sequence(
+				new Print(test)
+			);
+
+			var result = "";
+
+			using (var stream = new MemoryStream())
+			{
+				using (var sw = new StreamWriter(stream))
+				{
+					Console.SetOut(sw);
+
+					for (int i = 0; i < 10; ++i)
+					{
+						Assert.Equal(RoutineStatus.Success, sequence.Act(state));
+						state.Reset();
+
+						result += test + Environment.NewLine;
+					}
+				}
+
+				Assert.Equal(result, Encoding.UTF8.GetString(stream.ToArray()));
+			}
+		}
+	}
+}

bb880257a7aa7b831f6649ec40679fcc62b9b996
exec <exec@mabinoger.de>
2017-03-01 20:16:03 +0100

    Added MD5 quick access class

---
+++ Yggdrasil.Test/Security/Hashing/MD5.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see license file in the main folder
+
+using System.Text;
+using Xunit;
+using Yggdrasil.Security.Hashing;
+
+namespace Yggdrasil.Test.Security.Hashing
+{
+	public class MD5Tests
+	{
+		[Fact]
+		public void Encode()
+		{
+			Assert.Equal("81DC9BDB52D04DC20036DBD8313ED055", MD5.Encode("1234"));
+			Assert.Equal(new byte[] { 0x81, 0xDC, 0x9B, 0xDB, 0x52, 0xD0, 0x4D, 0xC2, 0x00, 0x36, 0xDB, 0xD8, 0x31, 0x3E, 0xD0, 0x55 }, MD5.Encode(Encoding.UTF8.GetBytes("1234")));
+
+			Assert.Equal("65A8E27D8879283831B664BD8B7F0AD4", MD5.Encode("Hello, World!"));
+			Assert.Equal(new byte[] { 0x65, 0xA8, 0xE2, 0x7D, 0x88, 0x79, 0x28, 0x38, 0x31, 0xB6, 0x64, 0xBD, 0x8B, 0x7F, 0x0A, 0xD4 }, MD5.Encode(Encoding.UTF8.GetBytes("Hello, World!")));
+		}
+	}
+}
---
+++ Yggdrasil/Security/Hashing/MD5.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see license file in the main folder
+
+using System;
+using System.Text;
+
+namespace Yggdrasil.Security.Hashing
+{
+	/// <summary>
+	/// Quick access to an MD5 encoder.
+	/// </summary>
+	public static class MD5
+	{
+		private readonly static System.Security.Cryptography.MD5 _md5 = System.Security.Cryptography.MD5.Create();
+
+		/// <summary>
+		/// Returns encoded value.
+		/// </summary>
+		/// <param name="val">Array to hash.</param>
+		/// <returns></returns>
+		public static byte[] Encode(byte[] val)
+		{
+			lock (_md5)
+				return _md5.ComputeHash(val);
+		}
+
+		/// <summary>
+		/// Returns encoded value.
+		/// </summary>
+		/// <param name="val">Value to hash.</param>
+		/// <returns></returns>
+		public static string Encode(string val)
+		{
+			var bytes = Encoding.UTF8.GetBytes(val);
+
+			lock (_md5)
+				bytes = _md5.ComputeHash(bytes);
+
+			return BitConverter.ToString(bytes).Replace("-", "").ToUpper();
+		}
+	}
+}

780788c0a437b2f356e599116cdf71d25a11cfb7
exec <exec@mabinoger.de>
2017-03-19 12:16:53 +0100

    Added StringBuilder extension

---
+++ Yggdrasil/Extensions/StringBuilder.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see license file in the main folder
+
+using System.Text;
+
+namespace Yggdrasil.Extensions
+{
+	public static class StringBuilderExtensions
+	{
+		/// <summary>
+		/// Appends the string returned by processing a composite format
+		/// string and the default line terminator to this instance. Each
+		/// format item is replaced by the string representation of a
+		/// corresponding argument in a parameter array.
+		/// </summary>
+		/// <param name="sb"></param>
+		/// <param name="format"></param>
+		/// <param name="args"></param>
+		/// <returns></returns>
+		public static StringBuilder AppendLine(this StringBuilder sb, string format, params object[] args)
+		{
+			sb.AppendFormat(format, args);
+			sb.AppendLine();
+
+			return sb;
+		}
+	}
+}

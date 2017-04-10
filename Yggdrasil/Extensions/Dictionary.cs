1852613775d7ebab181a70d36e07b67492f00d05
exec <exec@dragonbyte.de>
2017-04-10 15:19:46 +0200

    Added extension Dictionary.TryGetValue
    Without out parameter for one line usage.

---
+++ Yggdrasil/Extensions/Dictionary.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see licence.txt in the main folder
+
+using System.Collections.Generic;
+
+namespace Yggdrasil.Extensions
+{
+	public static class DictionaryExtensions
+	{
+		/// <summary>
+		/// Returns the value for the given key, or the value's default
+		/// value if the key wasn't found.
+		/// </summary>
+		/// <typeparam name="TKey"></typeparam>
+		/// <typeparam name="TValue"></typeparam>
+		/// <param name="dictionary"></param>
+		/// <param name="key"></param>
+		/// <returns></returns>
+		public static TValue TryGetValue<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key)
+		{
+			TValue result;
+			dictionary.TryGetValue(key, out result);
+			return result;
+		}
+	}
+}

788071a545dcc6e9d201b2dcbbea52859d03cc6a
exec <exec@dragonbyte.de>
2017-10-06 16:34:32 +0200

    Added Data.ApplicationData

---
+++ Yggdrasil.Data/ApplicationData.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see licence.txt in the main folder
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Yggdrasil.Data
+{
+	public class ApplicationData
+	{
+		private Dictionary<Type, IDatabase> _databases = new Dictionary<Type, IDatabase>();
+
+		/// <summary>
+		/// Creates new instance, loading all databases found in the
+		/// executing assembly.
+		/// </summary>
+		public ApplicationData()
+		{
+			this.AutoLoad();
+		}
+
+		/// <summary>
+		/// Loads all databases found in the executing assembly.
+		/// </summary>
+		private void AutoLoad()
+		{
+			var asm = Assembly.GetExecutingAssembly();
+			var types = asm.GetTypes();
+
+			foreach (var type in types.Where(a => !a.IsAbstract && !a.IsInterface && a.GetInterfaces().Contains(typeof(IDatabase))))
+			{
+				var instance = (IDatabase)Activator.CreateInstance(type);
+				_databases[type] = instance;
+			}
+		}
+
+		/// <summary>
+		/// Returns database with given type.
+		/// </summary>
+		/// <typeparam name="TDatabase"></typeparam>
+		/// <returns></returns>
+		public TDatabase Get<TDatabase>() where TDatabase : IDatabase
+		{
+			IDatabase result;
+			_databases.TryGetValue(typeof(TDatabase), out result);
+			return (TDatabase)result;
+		}
+	}
+}

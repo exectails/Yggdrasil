0b4d66f5b4aa2b78a1b818235dbec75a4191a745
exec <exec@dragonbyte.de>
2017-10-11 14:44:35 +0200

    Added partial WebSocketFramer
    ReceiveData hasn't been implemented yet.

---
+++ Yggdrasil.Network.WebSocket/WebSocketFramer.cs
@@ -0,0 +1,187 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see licence.txt in the main folder
+
+using System;
+using System.Net;
+using System.Text;
+using Yggdrasil.Network.Framing;
+
+namespace Yggdrasil.Network.WebSocket
+{
+	/// <summary>
+	/// Framer for WebSocket messages.
+	/// </summary>
+	public class WebSocketFramer : IMessageFramer
+	{
+		/// <summary>
+		/// En- or decodes value with the 4 byte mask.
+		/// </summary>
+		/// <param name="value"></param>
+		/// <param name="offsetVal"></param>
+		/// <param name="length"></param>
+		/// <param name="mask"></param>
+		/// <param name="offsetMask"></param>
+		/// <returns></returns>
+		public static byte[] EnDecode(byte[] value, int offsetVal, int length, byte[] mask, int offsetMask)
+		{
+			var result = new byte[length];
+
+			for (var i = 0; i < length; ++i)
+				result[i] = (byte)(value[offsetVal + i] ^ mask[offsetMask + (i % 4)]);
+
+			return result;
+		}
+
+		/// <summary>
+		/// Frames given bytes as binary data message without mask.
+		/// </summary>
+		/// <param name="message"></param>
+		/// <returns></returns>
+		public byte[] Frame(byte[] message)
+		{
+			return this.Frame(message, false, FrameOpCode.BinaryData);
+		}
+
+		/// <summary>
+		/// Frames given bytes as binary data message.
+		/// </summary>
+		/// <param name="message"></param>
+		/// <returns></returns>
+		public byte[] Frame(byte[] message, bool useMask)
+		{
+			return this.Frame(message, useMask, FrameOpCode.BinaryData);
+		}
+
+		/// <summary>
+		/// Frames given string as UTF8 text message without mask.
+		/// </summary>
+		/// <param name="message"></param>
+		/// <returns></returns>
+		public byte[] Frame(string message)
+		{
+			var bytes = Encoding.UTF8.GetBytes(message);
+			return this.Frame(bytes, false, FrameOpCode.UTF8TextData);
+		}
+
+		/// <summary>
+		/// Frames given string as UTF8 text message.
+		/// </summary>
+		/// <param name="message"></param>
+		/// <returns></returns>
+		public byte[] Frame(string message, bool useMask)
+		{
+			var bytes = Encoding.UTF8.GetBytes(message);
+			return this.Frame(bytes, useMask, FrameOpCode.UTF8TextData);
+		}
+
+		/// <summary>
+		/// Frames given bytes.
+		/// </summary>
+		/// <param name="message">Payload, accepts null as zero length payload.</param>
+		/// <param name="useMask">Whether to mask the payload.</param>
+		/// <param name="opCode">What op code is to be set.</param>
+		/// <returns></returns>
+		/// <exception cref="ArgumentException">
+		/// Thrown if given op code can't have a message, but one is given.
+		/// </exception>
+		public byte[] Frame(byte[] message, bool useMask, FrameOpCode opCode)
+		{
+			if (message != null && message.Length > 0 && opCode != FrameOpCode.BinaryData && opCode != FrameOpCode.UTF8TextData && opCode != FrameOpCode.Continuation)
+				throw new ArgumentException("Only data messages can have a payload.");
+
+			var payloadLength = (message != null ? message.Length : 0);
+			var payloadStart = 2;
+
+			// fin, rsv1, rsv2, rsv3, opcode, mask, payload length
+			var headerLength = sizeof(byte) * 2;
+
+			if (payloadLength <= 125)
+			{
+			}
+			else if (payloadLength <= short.MaxValue)
+			{
+				headerLength += sizeof(short); // extended payload length
+				payloadStart += sizeof(short);
+			}
+			else
+			{
+				headerLength += sizeof(long); // extended payload length 2
+				payloadStart += sizeof(long);
+			}
+
+			if (useMask)
+			{
+				headerLength += sizeof(byte) * 4;
+				payloadStart += sizeof(byte) * 4;
+			}
+
+			var result = new byte[headerLength + payloadLength];
+
+			result[0] |= 0b10000000; // fin, rsv1, rsv2, rsv3
+			result[0] |= (byte)((byte)opCode & ~0xF0); // opcode
+
+			if (useMask)
+				result[1] |= 0b10000000; // mask
+
+			// payload length
+			if (payloadLength <= 125)
+			{
+				result[1] |= (byte)payloadLength;
+			}
+			else if (payloadLength <= short.MaxValue)
+			{
+				result[1] |= 0b01111110;
+				var val = BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)payloadLength));
+				Buffer.BlockCopy(val, 0, result, 2, val.Length);
+			}
+			else
+			{
+				result[1] |= 0b01111111;
+				var val = BitConverter.GetBytes(IPAddress.HostToNetworkOrder((long)payloadLength));
+				Buffer.BlockCopy(val, 0, result, 2, val.Length);
+			}
+
+			// mask
+			if (useMask)
+			{
+				var mask = BitConverter.GetBytes(0x0A0B0C0D);
+				Buffer.BlockCopy(mask, 0, result, payloadStart - sizeof(int), mask.Length);
+
+				if (message != null)
+				{
+					var maskedPayload = EnDecode(message, 0, message.Length, mask, 0);
+					message = maskedPayload;
+				}
+			}
+
+			// payload
+			if (message != null)
+				Buffer.BlockCopy(message, 0, result, payloadStart, payloadLength);
+
+			return result;
+		}
+
+		/// <summary>
+		/// Receives data.
+		/// </summary>
+		/// <param name="data"></param>
+		/// <param name="length"></param>
+		public void ReceiveData(byte[] data, int length)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	/// <summary>
+	/// OP code for a WebSocket message.
+	/// </summary>
+	public enum FrameOpCode
+	{
+		Continuation = 0x00,
+		UTF8TextData = 0x01,
+		BinaryData = 0x02,
+		TerminateConnection = 0x08,
+		Ping = 0x09,
+		Pong = 0x0A,
+	}
+}
---
+++ Yggdrasil.Test/Network/WebSocketFramer.cs
@@ -0,0 +1,103 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see licence.txt in the main folder
+
+using System;
+using Xunit;
+using Yggdrasil.Network.WebSocket;
+
+namespace Yggdrasil.Test.Network
+{
+	public class WebSocketFramerTests
+	{
+		[Fact]
+		public void EnDecode()
+		{
+			var val = new byte[] { 1, 2, 3 };
+			var mask = new byte[] { 0x0A, 0x0B, 0x0C, 0x0D };
+			var encoded = WebSocketFramer.EnDecode(val, 0, val.Length, mask, 0);
+			var decoded = WebSocketFramer.EnDecode(encoded, 0, val.Length, mask, 0);
+			Assert.Equal(new byte[] { 1 ^ 0x0A, 2 ^ 0x0B, 3 ^ 0x0C }, encoded);
+			Assert.Equal(new byte[] { 1, 2, 3 }, decoded);
+
+			val = new byte[] { 1, 2, 3, 4, 5, 6 };
+			mask = new byte[] { 0x0A, 0x0B, 0x0C, 0x0D };
+			encoded = WebSocketFramer.EnDecode(val, 0, val.Length, mask, 0);
+			decoded = WebSocketFramer.EnDecode(encoded, 0, val.Length, mask, 0);
+			Assert.Equal(new byte[] { 1 ^ 0x0A, 2 ^ 0x0B, 3 ^ 0x0C, 4 ^ 0x0D, 5 ^ 0x0A, 6 ^ 0x0B }, encoded);
+			Assert.Equal(new byte[] { 1, 2, 3, 4, 5, 6 }, decoded);
+
+			var combined = new byte[] { 0, 0, 0, 1, 2, 3, 0x0A, 0x0B, 0x0C, 0x0D, 0, 0, 0 };
+			encoded = WebSocketFramer.EnDecode(combined, 3, 3, combined, 6);
+			decoded = WebSocketFramer.EnDecode(encoded, 0, 3, combined, 6);
+			Assert.Equal(new byte[] { 1 ^ 0x0A, 2 ^ 0x0B, 3 ^ 0x0C }, encoded);
+			Assert.Equal(new byte[] { 1, 2, 3 }, decoded);
+		}
+
+		[Fact]
+		public void FrameUnmasked()
+		{
+			var framer = new WebSocketFramer();
+
+			var buffer = framer.Frame(null, false, FrameOpCode.TerminateConnection);
+			Assert.Equal(new byte[] { 0x88, 0x00 }, buffer);
+
+			buffer = framer.Frame("123", false);
+			Assert.Equal(new byte[] { 0x81, 0x03, 0x31, 0x32, 0x33 }, buffer);
+
+			buffer = framer.Frame(new byte[] { 1, 2, 3 }, false);
+			Assert.Equal(new byte[] { 0x82, 0x03, 0x01, 0x02, 0x03 }, buffer);
+
+			buffer = framer.Frame(new byte[125], false);
+			var expected = new byte[2 + 125];
+			expected[0] = 0x82;
+			expected[1] = 0x7D;
+			Assert.Equal(expected, buffer);
+
+			buffer = framer.Frame(new byte[126], false);
+			expected = new byte[2 + 2 + 126];
+			expected[0] = 0x82;
+			expected[1] = 0x7E;
+			expected[2] = 0x00;
+			expected[3] = 0x7E;
+			Assert.Equal(expected, buffer);
+
+			buffer = framer.Frame(new byte[short.MaxValue + 10], false);
+			expected = new byte[2 + 8 + short.MaxValue + 10];
+			expected[0] = 0x82;
+			expected[1] = 0x7F;
+			expected[2] = 0x00;
+			expected[3] = 0x00;
+			expected[4] = 0x00;
+			expected[5] = 0x00;
+			expected[6] = 0x00;
+			expected[7] = 0x00;
+			expected[8] = 0x80;
+			expected[9] = 0x09;
+			Assert.Equal(expected, buffer);
+
+			Assert.Throws<ArgumentException>(() => framer.Frame(new byte[1], false, FrameOpCode.TerminateConnection));
+			Assert.Throws<ArgumentException>(() => framer.Frame(new byte[1], false, FrameOpCode.Ping));
+			Assert.Throws<ArgumentException>(() => framer.Frame(new byte[1], false, FrameOpCode.Pong));
+			Assert.DoesNotThrow(() => framer.Frame(new byte[1], false, FrameOpCode.BinaryData));
+			Assert.DoesNotThrow(() => framer.Frame(new byte[1], false, FrameOpCode.UTF8TextData));
+			Assert.DoesNotThrow(() => framer.Frame(new byte[1], false, FrameOpCode.Continuation));
+		}
+
+		[Fact]
+		public void FrameMasked()
+		{
+			var framer = new WebSocketFramer();
+
+			var val = new byte[] { 1, 2, 3 };
+			var buffer = framer.Frame(val, true);
+			var headerLength = 6;
+			Assert.Equal(headerLength + val.Length, buffer.Length);
+			Assert.Equal(0x82, buffer[0]);
+			Assert.Equal(0x83, buffer[1]);
+			Console.WriteLine(BitConverter.ToString(buffer));
+			Console.WriteLine(BitConverter.ToString(buffer, headerLength, val.Length));
+			Console.WriteLine(BitConverter.ToString(buffer, headerLength - 4, 4));
+			Assert.Equal(new byte[] { 1, 2, 3 }, WebSocketFramer.EnDecode(buffer, headerLength, val.Length, buffer, headerLength - 4));
+		}
+	}
+}

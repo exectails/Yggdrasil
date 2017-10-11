1215e56de037de5a62939ca5c0a232e96266496a
exec <exec@dragonbyte.de>
2017-10-11 16:08:23 +0200

    Added WebSocketFrame
    Basically a parser for the data that WebSocketFramer returns.

---
+++ Yggdrasil.Network.WebSocket/WebSocketFrame.cs
@@ -0,0 +1,84 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see licence.txt in the main folder
+
+using System;
+using System.Net;
+
+namespace Yggdrasil.Network.WebSocket
+{
+	/// <summary>
+	/// Represents a frame received via WebSocket.
+	/// </summary>
+	public class WebSocketFrame
+	{
+		/// <summary>
+		/// Specifies whether this frame was the finale one for the
+		/// message.
+		/// </summary>
+		public bool Fin { get; }
+
+		/// <summary>
+		/// Returns the frames op-code.
+		/// </summary>
+		public FrameOpCode OpCode { get; }
+
+		/// <summary>
+		/// Returns the frame's data.
+		/// </summary>
+		public byte[] PayLoad { get; }
+
+		/// <summary>
+		/// Creates a new instance, based on buffer.
+		/// </summary>
+		/// <param name="buffer"></param>
+		public WebSocketFrame(byte[] buffer)
+		{
+			this.Fin = (buffer[0] & 0b10000000) != 0;
+			this.OpCode = (FrameOpCode)(buffer[0] & ~0b11110000);
+			this.PayLoad = ParsePayload(buffer);
+		}
+
+		/// <summary>
+		/// Reads the payload from the buffer and returns it.
+		/// </summary>
+		/// <param name="buffer"></param>
+		/// <returns></returns>
+		private static byte[] ParsePayload(byte[] buffer)
+		{
+			var payloadStart = sizeof(byte) * 2;
+			var payloadLength = 0;
+
+			var usesMask = (buffer[1] & 0b10000000) != 0;
+			if (usesMask)
+				payloadStart += sizeof(byte) * 4;
+
+			var lenCode = (buffer[1] & ~0b10000000);
+			if (lenCode <= 125)
+			{
+				payloadStart += 0;
+				payloadLength = lenCode;
+			}
+			else if (lenCode == 126)
+			{
+				payloadStart += sizeof(short);
+				payloadLength = IPAddress.NetworkToHostOrder(BitConverter.ToInt16(buffer, sizeof(byte) * 2));
+			}
+			else if (lenCode == 127)
+			{
+				payloadStart += sizeof(long);
+				payloadLength = (int)IPAddress.NetworkToHostOrder(BitConverter.ToInt64(buffer, sizeof(byte) * 2));
+			}
+
+			var result = new byte[payloadLength];
+			Buffer.BlockCopy(buffer, payloadStart, result, 0, payloadLength);
+
+			if (usesMask)
+			{
+				var maskStart = payloadStart - sizeof(int);
+				WebSocketFramer.EnDecode(ref result, 0, payloadLength, buffer, maskStart);
+			}
+
+			return result;
+		}
+	}
+}
---
+++ Yggdrasil.Test/Network/WebSocketFrame.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see licence.txt in the main folder
+
+using System.Text;
+using Xunit;
+using Yggdrasil.Network.WebSocket;
+
+namespace Yggdrasil.Test.Network
+{
+	public class WebSocketFrameTests
+	{
+		[Fact]
+		public void InitUnmasked()
+		{
+			var framer = new WebSocketFramer(1024);
+
+			var buffer = framer.Frame(new byte[] { 4, 5, 6 }, false);
+			var frame = new WebSocketFrame(buffer);
+			Assert.Equal(true, frame.Fin);
+			Assert.Equal(FrameOpCode.BinaryData, frame.OpCode);
+			Assert.Equal(new byte[] { 4, 5, 6 }, frame.PayLoad);
+
+			buffer = framer.Frame("test", false);
+			frame = new WebSocketFrame(buffer);
+			Assert.Equal(true, frame.Fin);
+			Assert.Equal(FrameOpCode.UTF8TextData, frame.OpCode);
+			Assert.Equal(Encoding.UTF8.GetBytes("test"), frame.PayLoad);
+		}
+
+		[Fact]
+		public void InitMasked()
+		{
+			var framer = new WebSocketFramer(1024);
+
+			var buffer = framer.Frame(new byte[] { 4, 5, 6 }, true);
+			var frame = new WebSocketFrame(buffer);
+			Assert.Equal(true, frame.Fin);
+			Assert.Equal(FrameOpCode.BinaryData, frame.OpCode);
+			Assert.Equal(new byte[] { 4, 5, 6 }, frame.PayLoad);
+
+			buffer = framer.Frame("test", true);
+			frame = new WebSocketFrame(buffer);
+			Assert.Equal(true, frame.Fin);
+			Assert.Equal(FrameOpCode.UTF8TextData, frame.OpCode);
+			Assert.Equal(Encoding.UTF8.GetBytes("test"), frame.PayLoad);
+		}
+	}
+}

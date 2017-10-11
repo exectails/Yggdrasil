ce0c2c298c5f1e2e6c47d9d4da7207149e171dee
exec <exec@dragonbyte.de>
2017-10-11 20:50:10 +0200

    Added DoubleNewLineFramer

---
+++ Yggdrasil.Test/Network/DoubleNewLineFramer.cs
@@ -0,0 +1,124 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see license file in the main folder
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Yggdrasil.Network.Framing;
+
+namespace Yggdrasil.Test.Network
+{
+	public class DoubleNewLineFramerTests
+	{
+		[Fact]
+		public void Frame()
+		{
+			var maxMessageSize = 10;
+			var framer = new DoubleNewLineFramer(maxMessageSize);
+
+			// Check result
+			var message = new byte[] { 1, 2, 3, 4, 5 };
+			Assert.Equal(new byte[] { 1, 2, 3, 4, 5, (byte)'\r', (byte)'\n', (byte)'\r', (byte)'\n' }, framer.Frame(message));
+
+			var messageStr = "12345";
+			Assert.Equal(Encoding.UTF8.GetBytes("12345\r\n\r\n"), framer.Frame(messageStr));
+
+			// Check max size exception
+			var invalidMessage = new byte[maxMessageSize + 1];
+			Assert.Throws<InvalidMessageSizeException>(() => framer.Frame(invalidMessage));
+		}
+
+		[Fact]
+		public void ReceiveData_Single()
+		{
+			var receivedMessages = new List<string>();
+
+			var maxMessageSize = 10;
+			var framer = new DoubleNewLineFramer(maxMessageSize);
+			framer.MessageReceived += (msg => receivedMessages.Add(msg));
+
+			var message = "12345";
+			var data = framer.Frame(message);
+			framer.ReceiveData(data, data.Length);
+
+			Assert.Equal(1, receivedMessages.Count);
+			Assert.Equal(message, receivedMessages[0]);
+		}
+
+		[Fact]
+		public void ReceiveData_Multiple()
+		{
+			var receivedMessages = new List<string>();
+
+			var maxMessageSize = 10;
+			var framer = new DoubleNewLineFramer(maxMessageSize);
+			framer.MessageReceived += (msg => receivedMessages.Add(msg));
+
+			// Create messages
+			var message1 = "12345";
+			var message2 = "567";
+			var message3 = "foobar";
+
+			// Put messages into one array
+			var dataList = new List<byte>();
+			dataList.AddRange(framer.Frame(message1));
+			dataList.AddRange(framer.Frame(message2));
+			dataList.AddRange(framer.Frame(message3));
+
+			// Receive the array
+			var data = dataList.ToArray();
+			framer.ReceiveData(data, data.Length);
+
+			// Check
+			Assert.Equal(3, receivedMessages.Count);
+			Assert.Equal(message1, receivedMessages[0]);
+			Assert.Equal(message2, receivedMessages[1]);
+			Assert.Equal(message3, receivedMessages[2]);
+		}
+
+		[Fact]
+		public void ReceiveData_Fragmented()
+		{
+			var receivedMessages = new List<string>();
+
+			var maxMessageSize = 10;
+			var framer = new DoubleNewLineFramer(maxMessageSize);
+			framer.MessageReceived += (msg => receivedMessages.Add(msg));
+
+			// Check receiving a different amount of bytes each time
+			foreach (var steps in new int[] { 1, 2, 3, 4, 5 })
+			{
+				// Create messages
+				var message1 = "12345";
+				var message2 = "567";
+				var message3 = "foobar";
+
+				// Put messages into one array
+				var dataList = new List<byte>();
+				dataList.AddRange(framer.Frame(message1));
+				dataList.AddRange(framer.Frame(message2));
+				dataList.AddRange(framer.Frame(message3));
+
+				// Receive data step by step
+				for (var i = 0; i < dataList.Count; i += steps)
+				{
+					var take = Math.Min(dataList.Count - i, steps);
+					if (take < 0)
+						continue;
+
+					var data = dataList.Skip(i).Take(take).ToArray();
+					framer.ReceiveData(data, data.Length);
+				}
+
+				// Check
+				Assert.Equal(3, receivedMessages.Count);
+				Assert.Equal(message1, receivedMessages[0]);
+				Assert.Equal(message2, receivedMessages[1]);
+				Assert.Equal(message3, receivedMessages[2]);
+				receivedMessages.Clear();
+			}
+		}
+	}
+}
---
+++ Yggdrasil/Network/Framing/DoubleNewLineFramer.cs
@@ -0,0 +1,101 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see license file in the main folder
+
+using System;
+using System.Text;
+
+namespace Yggdrasil.Network.Framing
+{
+	public class DoubleNewLineFramer : IMessageFramer
+	{
+		private byte[] _buffer;
+		private int _bytesReceived;
+
+		/// <summary>
+		/// Maximum size of messages.
+		/// </summary>
+		public int MaxMessageSize { get; private set; }
+
+		/// <summary>
+		/// Called every time ReceiveData got a full message.
+		/// </summary>
+		public event Action<string> MessageReceived;
+
+		/// <summary>
+		/// Creates new instance.
+		/// </summary>
+		/// <param name="maxMessageSize">Maximum size of messages</param>
+		public DoubleNewLineFramer(int maxMessageSize)
+		{
+			this.MaxMessageSize = maxMessageSize;
+			_buffer = new byte[this.MaxMessageSize];
+		}
+
+		/// <summary>
+		/// Appends terminating double-new-line to message.
+		/// </summary>
+		/// <param name="message"></param>
+		/// <returns></returns>
+		public byte[] Frame(byte[] message)
+		{
+			var messageLength = message.Length;
+			if (messageLength > this.MaxMessageSize)
+				throw new InvalidMessageSizeException("Invalid size (" + messageLength + ").");
+
+			var result = new byte[message.Length + 4];
+			Buffer.BlockCopy(message, 0, result, 0, message.Length);
+			result[result.Length - 4] = (byte)'\r';
+			result[result.Length - 3] = (byte)'\n';
+			result[result.Length - 2] = (byte)'\r';
+			result[result.Length - 1] = (byte)'\n';
+
+			return result;
+		}
+
+		/// <summary>
+		/// Appends terminating double-new-line to message.
+		/// </summary>
+		/// <param name="message"></param>
+		/// <returns></returns>
+		public byte[] Frame(string message)
+		{
+			return this.Frame(Encoding.UTF8.GetBytes(message));
+		}
+
+		/// <summary>
+		/// Receives data and calls MessageReceived every time a full message
+		/// has arrived.
+		/// </summary>
+		/// <param name="data">Buffer to read from.</param>
+		/// <param name="length">Length of actual information in data.</param>
+		/// <exception cref="InvalidMessageSizeException">
+		/// Thrown if a message has an invalid size. Should this occur,
+		/// the connection should be terminated, because it's not save to
+		/// keep receiving anymore.
+		/// </exception>
+		public void ReceiveData(byte[] data, int length)
+		{
+			var bytesAvailable = length;
+			if (bytesAvailable == 0)
+				return;
+
+			for (var i = 0; i < bytesAvailable; ++i)
+			{
+				_buffer[_bytesReceived] = data[i];
+				_bytesReceived++;
+
+				if (_bytesReceived > this.MaxMessageSize)
+					throw new InvalidMessageSizeException("Invalid size (" + _bytesReceived + ").");
+
+				var end = (_bytesReceived >= 4 && _buffer[_bytesReceived - 4] == (byte)'\r' && _buffer[_bytesReceived - 3] == (byte)'\n' && _buffer[_bytesReceived - 2] == (byte)'\r' && _buffer[_bytesReceived - 1] == (byte)'\n');
+
+				if (end)
+				{
+					var message = Encoding.UTF8.GetString(_buffer, 0, _bytesReceived - 4);
+					this.MessageReceived?.Invoke(message);
+					_bytesReceived = 0;
+				}
+			}
+		}
+	}
+}

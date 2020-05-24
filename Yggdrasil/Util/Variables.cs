38ad13e170d043a1f4b655dff3e0161545fa9f80
exec <exec@dragonbyte.de>
2020-05-24 22:14:02 +0200

    Removed Variables serialization
    Too application specific to keep in the lib.

--- Yggdrasil/Util/Variables.cs
+++ Yggdrasil/Util/Variables.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.Text;
-using System.Text.RegularExpressions;
 
 namespace Yggdrasil.Util
 {
@@ -10,19 +7,8 @@ namespace Yggdrasil.Util
 	/// Wrapper around a dictionary of objects that can be read from and
 	/// serializes to a single string.
 	/// </summary>
-	/// <example>
-	/// var dict = Variables.FromString("SOMEINT:4:1234;SOMESTR:s:test;");
-	/// var someInt = dict.GetInt("SOMEINT"); // 1234
-	/// 
-	/// var dict = new Variables();
-	/// dict.SetInt("SOMEOTHERINT", 3456);
-	/// var str = dict.ToString(); // SOMEOTHERINT:4:3456;
-	/// </example>
 	public class Variables
 	{
-		private static readonly Regex _validityRegex = new Regex("^([^:]+:[^:]+:[^;]*;)+$", RegexOptions.Compiled);
-		private static readonly Regex _valueRegex = new Regex("([^:]+):([^:]+):([^;]*);", RegexOptions.Compiled);
-
 		private readonly object _syncLock = new object();
 		private Dictionary<string, object> _variables = new Dictionary<string, object>();
 		private string _cache = null;
@@ -52,19 +38,14 @@ namespace Yggdrasil.Util
 		}
 
 		/// <summary>
-		/// Creates new instance from the string of values.
+		/// Returns true if a variable with the given name exists.
 		/// </summary>
-		/// <param name="values"></param>
+		/// <param name="name"></param>
 		/// <returns></returns>
-		public static Variables FromString(string values)
+		public bool Has(string name)
 		{
-			if (!_validityRegex.IsMatch(values))
-				throw new ArgumentException("Invalid string format.");
-
-			var dict = new Variables();
-			dict.Parse(values);
-
-			return dict;
+			lock (_syncLock)
+				return _variables.ContainsKey(name);
 		}
 
 		/// <summary>
@@ -401,119 +382,6 @@ namespace Yggdrasil.Util
 			{
 				foreach (var variable in variables)
 					_variables[variable.Key] = variable.Value;
-			}
-		}
-
-		/// <summary>
-		/// Returns string type identifier for the value, returns null
-		/// if the type is not supported.
-		/// </summary>
-		/// <param name="value"></param>
-		/// <returns></returns>
-		protected virtual string GetTypeIdent(object value)
-		{
-			var typeCode = Type.GetTypeCode(value.GetType());
-
-			switch (typeCode)
-			{
-				case TypeCode.Byte:
-				case TypeCode.SByte: return "1";
-				case TypeCode.Int16:
-				case TypeCode.UInt16: return "2";
-				case TypeCode.Int32:
-				case TypeCode.UInt32: return "4";
-				case TypeCode.Int64:
-				case TypeCode.UInt64: return "8";
-				case TypeCode.Single: return "f";
-				case TypeCode.Double: return "d";
-				case TypeCode.String: return "s";
-				case TypeCode.Boolean: return "b";
-
-				case TypeCode.Object:
-					if (value is byte[])
-						return "B";
-					break;
-			}
-
-			return null;
-		}
-
-		/// <summary>
-		/// Returns dictionary in the format "key:varType:value;...".
-		/// </summary>
-		/// <returns></returns>
-		/// <exception cref="InvalidOperationException">
-		/// Thrown if instance contains a variable that can't be represented
-		/// as a string.
-		/// </exception>
-		public override string ToString()
-		{
-			var sb = new StringBuilder();
-
-			lock (_syncLock)
-			{
-				if (_variables.Count < 1)
-					return string.Empty;
-
-				if (_cache != null)
-					return _cache;
-
-				foreach (var variable in _variables)
-				{
-					var sType = this.GetTypeIdent(variable.Value);
-					if (sType == null)
-						throw new ArgumentException($"Unsupported type '{variable.Value.GetType().Name}' on '{variable.Key}'.");
-
-					if (sType == "b")
-						sb.AppendFormat("{0}:{1}:{2};", variable.Key, sType, (bool)variable.Value ? "1" : "0");
-					else if (sType == "s")
-						sb.AppendFormat("{0}:{1}:{2};", variable.Key, sType, ((string)variable.Value).Replace(";", "%S").Replace(":", "%C"));
-					else if (sType == "f")
-						sb.AppendFormat("{0}:{1}:{2};", variable.Key, sType, ((float)variable.Value).ToString(CultureInfo.InvariantCulture));
-					else if (sType == "d")
-						sb.AppendFormat("{0}:{1}:{2};", variable.Key, sType, ((double)variable.Value).ToString(CultureInfo.InvariantCulture));
-					else if (sType == "B")
-						sb.AppendFormat("{0}:{1}:{2};", variable.Key, sType, Convert.ToBase64String((byte[])variable.Value));
-					else
-						sb.AppendFormat("{0}:{1}:{2};", variable.Key, sType, variable.Value);
-				}
-
-				return (_cache = sb.ToString());
-			}
-		}
-
-		/// <summary>
-		/// Reads a string in the format "key:varType:value;..." and adds
-		/// the values to this tag collection.
-		/// </summary>
-		/// <param name="str"></param>
-		public virtual void Parse(string str)
-		{
-			if (str == null)
-				throw new ArgumentNullException(nameof(str));
-
-			str = str.Trim();
-			if (str == "")
-				return;
-
-			foreach (Match match in _valueRegex.Matches(str))
-			{
-				var name = match.Groups[1].Value;
-				var type = match.Groups[2].Value;
-				var value = match.Groups[3].Value;
-
-				switch (type)
-				{
-					case "1": this.Set(name, Convert.ToByte(value)); break;
-					case "2": this.Set(name, Convert.ToInt16(value)); break;
-					case "4": this.Set(name, Convert.ToInt32(value)); break;
-					case "8": this.Set(name, Convert.ToInt64(value)); break;
-					case "f": this.Set(name, Convert.ToSingle(value, CultureInfo.InvariantCulture)); break;
-					case "d": this.Set(name, Convert.ToDouble(value, CultureInfo.InvariantCulture)); break;
-					case "s": this.Set(name, value.Replace("%S", ";").Replace("%C", ":")); break;
-					case "b": this.Set(name, value == "1"); break;
-					case "B": this.Set(name, Convert.FromBase64String(value)); break;
-				}
 			}
 		}
 	}

4026f76468ca2aa1d42d9d2cab4ecf78ec600ef2
exec <exec@dragonbyte.de>
2021-09-15 11:20:14 +0200

    Added component classes

---
+++ Yggdrasil.Test/EntityComponentSystem/Components.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Xunit;
+using Yggdrasil.EntityComponentSystem;
+
+namespace Yggdrasil.Test.EntityComponentSystem
+{
+	public class Components
+	{
+		[Fact]
+		public void Add()
+		{
+			var components = new ComponentCollection();
+
+			components.Add(new Component1());
+			Assert.Equal(255, components.Get<Component1>().Foo);
+		}
+
+		[Fact]
+		public void Remove()
+		{
+			var components = new ComponentCollection();
+
+			Assert.False(components.TryGet<Component1>(out _));
+			Assert.Null(components.Get<Component1>());
+
+			components.Add(new Component1());
+			Assert.True(components.TryGet<Component1>(out _));
+			Assert.Equal(255, components.Get<Component1>().Foo);
+
+			components.Remove(components.Get<Component1>());
+			Assert.False(components.TryGet<Component1>(out _));
+			Assert.Null(components.Get<Component1>());
+		}
+
+		[Fact]
+		public void Update()
+		{
+			var components = new ComponentCollection();
+
+			components.Add(new Component1());
+			Assert.Equal(255, components.Get<Component1>().Foo);
+
+			for (var i = 0; i < 5; ++i)
+			{
+				components.Update(TimeSpan.FromMilliseconds(16.7));
+			}
+
+			Assert.Equal(260, components.Get<Component1>().Foo);
+		}
+
+		private class Component1 : IUpdatableComponent
+		{
+			public int Foo { get; private set; } = 255;
+
+			public void Update(TimeSpan elapsed)
+			{
+				this.Foo++;
+			}
+		}
+	}
+}
---
+++ Yggdrasil/EntityComponentSystem/Component.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Yggdrasil.EntityComponentSystem
+{
+	/// <summary>
+	/// Base interface for components.
+	/// </summary>
+	public interface IComponent
+	{
+	}
+
+	/// <summary>
+	/// Base interface for updatable components.
+	/// </summary>
+	public interface IUpdatableComponent : IComponent
+	{
+		/// <summary>
+		/// Updates component.
+		/// </summary>
+		/// <param name="elapsed"></param>
+		void Update(TimeSpan elapsed);
+	}
+}

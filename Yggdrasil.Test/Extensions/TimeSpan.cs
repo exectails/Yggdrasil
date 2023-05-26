2824840dbbcc75f70a1e5c969ae84deee87a2c54
exec <exec@dragonbyte.de>
2023-05-26 09:38:00 +0200

    Added Divide and Multiply extensions for TimeSpan

---
+++ Yggdrasil.Test/Extensions/TimeSpan.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Xunit;
+using Yggdrasil.Extensions;
+
+namespace Yggdrasil.Test.Extensions
+{
+	public class TimeSpanTests
+	{
+		[Fact]
+		public void Divide()
+		{
+			var ts = TimeSpan.FromSeconds(10);
+
+			Assert.Equal(TimeSpan.FromSeconds(5), ts.Divide(2));
+			Assert.Equal(TimeSpan.FromSeconds(2), ts.Divide(5));
+			Assert.Equal(TimeSpan.FromSeconds(1), ts.Divide(10));
+
+			Assert.Equal(TimeSpan.FromMilliseconds(5000), ts.Divide(2));
+			Assert.Equal(TimeSpan.FromMilliseconds(2000), ts.Divide(5));
+			Assert.Equal(TimeSpan.FromMilliseconds(1000), ts.Divide(10));
+		}
+
+		[Fact]
+		public void Multiply()
+		{
+			var ts = TimeSpan.FromSeconds(10);
+
+			Assert.Equal(TimeSpan.FromSeconds(20), ts.Multiply(2));
+			Assert.Equal(TimeSpan.FromSeconds(50), ts.Multiply(5));
+			Assert.Equal(TimeSpan.FromSeconds(100), ts.Multiply(10));
+
+			Assert.Equal(TimeSpan.FromMilliseconds(20000), ts.Multiply(2));
+			Assert.Equal(TimeSpan.FromMilliseconds(50000), ts.Multiply(5));
+			Assert.Equal(TimeSpan.FromMilliseconds(100000), ts.Multiply(10));
+
+			Assert.Equal(TimeSpan.FromMinutes(1), ts.Multiply(6));
+			Assert.Equal(TimeSpan.FromMinutes(5), ts.Multiply(30));
+		}
+	}
+}
---
+++ Yggdrasil/Extensions/TimeSpan.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Yggdrasil.Extensions
+{
+	/// <summary>
+	/// Extensions for TimeSpan.
+	/// </summary>
+	public static class TimeSpanExtensions
+	{
+		/// <summary>
+		/// Divides the timespan by the divisor and returns the result.
+		/// </summary>
+		/// <param name="timespan"></param>
+		/// <param name="divisor"></param>
+		/// <returns></returns>
+		public static TimeSpan Divide(this TimeSpan timespan, int divisor)
+			=> TimeSpan.FromTicks(timespan.Ticks / divisor);
+
+		/// <summary>
+		/// Multiplies the timespan by the multiplier and returns the result.
+		/// </summary>
+		/// <param name="timespan"></param>
+		/// <param name="multiplier"></param>
+		/// <returns></returns>
+		public static TimeSpan Multiply(this TimeSpan timespan, int multiplier)
+			=> TimeSpan.FromTicks(timespan.Ticks * multiplier);
+	}
+}

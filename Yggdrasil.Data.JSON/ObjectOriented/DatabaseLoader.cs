af5f1b22baf54da54286a2dbddf550841f5392dd
Exec Tails <exec@dragonbyte.de>
2024-10-24 23:10:49 +0200

    Reorganized object database types
    Turned ObjectDatabase into a more generic database loader that leaves the handling of the entries to its children. This decouples the loading from the whole id/versioning thing and makes it easier for users to create databases that don't follow the intended standard setup.

---
+++ Yggdrasil.Data.JSON/ObjectOriented/DatabaseLoader.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Yggdrasil.Data.JSON.ObjectOriented
+{
+	/// <summary>
+	/// Loads database entries from a file or stream.
+	/// </summary>
+	public abstract class DatabaseLoader
+	{
+		/// <summary>
+		/// Returns the warnings that were encountered during the last call to
+		/// Load.
+		/// </summary>
+		public List<DatabaseWarningException> Warnings { get; } = new List<DatabaseWarningException>();
+
+		/// <summary>
+		/// Loads the database from the given file path.
+		/// </summary>
+		/// <param name="filePath"></param>
+		public void Load(string filePath)
+		{
+			using (var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+				this.Load(filePath, fs);
+		}
+
+		/// <summary>
+		/// Loads the database entries from the given stream.
+		/// </summary>
+		/// <param name="fileName">File name to use as reference in case of errors.</param>
+		/// <param name="stream">Stream to load data from.</param>
+		public void Load(string fileName, Stream stream)
+		{
+			this.Warnings.Clear();
+
+			using (var sr = new StreamReader(stream))
+			using (var reader = new JsonTextReader(sr))
+			{
+				try
+				{
+					var array = JArray.Load(reader);
+
+					foreach (var token in array)
+					{
+						if (!(token is JObject jObj))
+							continue;
+
+						try
+						{
+							this.HandleEntry(jObj);
+						}
+						catch (MandatoryValueException ex)
+						{
+							this.Warnings.Add(new MandatoryValueException(fileName, ex.Key, jObj));
+							continue;
+						}
+						catch (DatabaseWarningException ex)
+						{
+							var msg = string.Format("{0}\n{1}", ex.Message, jObj);
+
+							this.Warnings.Add(new DatabaseWarningException(fileName, msg));
+							continue;
+						}
+						catch (OverflowException)
+						{
+							var msg = string.Format("Number too big or too small for variable, in \n{0}", jObj);
+
+							this.Warnings.Add(new DatabaseWarningException(fileName, msg));
+							continue;
+						}
+						catch (Exception ex)
+						{
+							var msg = string.Format("Exception: {0}\nEntry: \n{1}", ex, jObj);
+
+							throw new DatabaseErrorException(fileName, msg);
+						}
+					}
+				}
+				catch (JsonReaderException ex)
+				{
+					// Throw to stop the server, databases depend on each
+					// other, skipping one could lead to problems.
+					throw new DatabaseErrorException(fileName, ex.Message);
+				}
+			}
+
+			this.AfterLoad();
+		}
+
+		/// <summary>
+		/// Handles a single entry read from the database.
+		/// </summary>
+		/// <param name="jObj"></param>
+		protected abstract void HandleEntry(JObject jObj);
+
+		/// <summary>
+		/// Called after all entries were loaded and handled.
+		/// </summary>
+		protected virtual void AfterLoad()
+		{
+		}
+	}
+}
--- Yggdrasil.Data.JSON/ObjectOriented/StandardObjectDatabase.cs
+++ Yggdrasil.Data.JSON/ObjectOriented/StandardObjectDatabase.cs
@@ -24,19 +24,121 @@ namespace Yggdrasil.Data.JSON.ObjectOriented
 	/// integer id and determines overrideability by a numeric version.
 	/// </summary>
 	/// <typeparam name="TObject"></typeparam>
-	public abstract class StandardObjectDatabase<TObject> : ObjectDatabase<int, int, TObject> where TObject : StandardObjectData, new()
+	public abstract class StandardObjectDatabase<TObject> : DatabaseLoader, IDatabase where TObject : StandardObjectData, new()
 	{
+		int IDatabase.Count => this.Objects.Count;
+		void IDatabase.Clear() => this.Objects.Clear();
+		void IDatabase.LoadFile(string filePath) => this.Load(filePath);
+		DatabaseWarningException[] IDatabase.GetWarnings() => this.Warnings.ToArray();
+
+		/// <summary>
+		/// Returns the target version of the database.
+		/// </summary>
+		public int Version { get; }
+
+		/// <summary>
+		/// Returns the loaded objects.
+		/// </summary>
+		public ObjectStorage<int, TObject> Objects { get; } = new ObjectStorage<int, TObject>();
+
+		/// <summary>
+		/// Creates a new object database.
+		/// </summary>
+		public StandardObjectDatabase() : this(default)
+		{
+		}
+
+		/// <summary>
+		/// Creates a new object database with the given target version.
+		/// </summary>
+		/// <param name="version"></param>
+		public StandardObjectDatabase(int version)
+		{
+			this.Version = version;
+		}
+
+		/// <summary>
+		/// Handles a single entry read from the database.
+		/// </summary>
+		/// <param name="jObj"></param>
+		protected override void HandleEntry(JObject jObj)
+		{
+			var newObj = new TObject();
+			this.ReadBaseData(jObj, newObj);
+
+			if (this.Objects.TryGet(newObj.Id, out var existingObj))
+			{
+				if (this.ShouldOverride(newObj, existingObj))
+				{
+					this.ReadEntry(jObj, newObj, existingObj);
+					this.InsertObject(newObj);
+				}
+			}
+			else
+			{
+				this.CheckFields(jObj);
+				this.ReadEntry(jObj, newObj, newObj);
+				this.InsertObject(newObj);
+			}
+		}
+
 		/// <summary>
 		/// Reads the base data from the entry and sets it on the given object.
 		/// </summary>
 		/// <param name="entry"></param>
 		/// <param name="dataObj"></param>
-		protected override void ReadBaseData(JObject entry, TObject dataObj)
+		protected void ReadBaseData(JObject entry, TObject dataObj)
 		{
 			entry.AssertNotMissing("id");
 
 			dataObj.Id = entry.ReadInt("id");
 			dataObj.Version = entry.ReadInt("version", 0);
+		}
+
+		/// <summary>
+		/// Returns true if the new object should override the existing one.
+		/// </summary>
+		/// <remarks>
+		/// Called for existing objects after the base data for the new object
+		/// was read. The given new object does not represent a fully loaded
+		/// object.
+		/// </remarks>
+		/// <param name="newObj"></param>
+		/// <param name="existingObj"></param>
+		/// <returns></returns>
+		protected virtual bool ShouldOverride(TObject newObj, TObject existingObj)
+		{
+			var existingVersion = existingObj.Version;
+			var newVersion = newObj.Version;
+
+			return (newVersion.CompareTo(this.Version) <= 0 && newVersion.CompareTo(existingVersion) >= 0);
+		}
+
+		/// <summary>
+		/// Returns true if the entry's fields are valid in number.
+		/// </summary>
+		/// <param name="entry"></param>
+		/// <returns></returns>
+		protected virtual void CheckFields(JObject entry)
+		{
+		}
+
+		/// <summary>
+		/// Reads an entry's data from the database into the object.
+		/// </summary>
+		/// <param name="entry"></param>
+		/// <param name="dataObj"></param>
+		/// <param name="existingObj"></param>
+		protected abstract void ReadEntry(JObject entry, TObject dataObj, TObject existingObj);
+
+		/// <summary>
+		/// Inserts the object into the database, potentially replacing existing
+		/// objects.
+		/// </summary>
+		/// <param name="obj"></param>
+		protected virtual void InsertObject(TObject obj)
+		{
+			this.Objects.Insert(obj);
 		}
 	}
 }

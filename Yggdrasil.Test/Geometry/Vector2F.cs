5de2458acefaf7d85c102780b0e61245e4bb1f70
Exec Tails <exec@dragonbyte.de>
2026-03-29 14:43:05 +0200

    Added some more vector unit tests

--- Yggdrasil.Test/Geometry/Vector2F.cs
+++ Yggdrasil.Test/Geometry/Vector2F.cs
@@ -32,5 +32,37 @@ namespace Yggdrasil.Test.Geometry
 			vector3 /= 4;
 			Assert.Equal(new Vector2F(25, 25), vector3);
 		}
+
+		[Fact]
+		public void GetDistance()
+		{
+			var vector1 = new Vector2F(0, 0);
+			var vector2 = new Vector2F(3, 4);
+
+			Assert.Equal(5, vector1.GetDistance(vector2));
+			Assert.Equal(5, vector2.GetDistance(vector1));
+
+			Assert.Equal(0, vector1.GetDistance(vector1));
+
+			var vector3 = new Vector2F(-3, -4);
+			Assert.Equal(5, vector1.GetDistance(vector3));
+			Assert.Equal(10, vector2.GetDistance(vector3));
+		}
+
+		[Fact]
+		public void InRange()
+		{
+			var vector1 = new Vector2F(0, 0);
+			var vector2 = new Vector2F(3, 4);
+
+			Assert.True(vector1.InRange(vector2, 5));
+			Assert.False(vector1.InRange(vector2, 4.9f));
+
+			Assert.True(vector1.InRange(vector1, 0));
+
+			var vector3 = new Vector2F(10, 0);
+			Assert.True(vector1.InRange(vector3, 10));
+			Assert.False(vector1.InRange(vector3, 9));
+		}
 	}
 }

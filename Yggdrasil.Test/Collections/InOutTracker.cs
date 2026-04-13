21ed63df1484423f95cd52ed218d7e8314de7388
Exec Tails <exec@dragonbyte.de>
2026-04-13 15:56:31 +0200

    Added InOutTracker

---
+++ Yggdrasil.Test/Collections/InOutTracker.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+using Xunit;
+using Yggdrasil.Collections;
+using Yggdrasil.Geometry;
+
+namespace Yggdrasil.Test.Collections
+{
+	public class InOutTrackerTests
+	{
+		[Fact]
+		public void AddAndRemoveItems()
+		{
+			var character1 = new Character("Character1", new Vector2F(50, 50));
+			var character2 = new Character("Character2", new Vector2F(40, 40));
+			var character3 = new Character("Character3", new Vector2F(100, 100));
+
+			var map = new Map();
+			map.AddCharacter(character1);
+			map.AddCharacter(character2);
+			map.AddCharacter(character3);
+
+			character1.Update(out var added, out var removed);
+
+			Assert.Equal(["Character2"], added);
+			Assert.Equal([], removed);
+
+			character2.Position = new Vector2F(0, 0);
+			character1.Update(out added, out removed);
+
+			Assert.Equal([], added);
+			Assert.Equal(["Character2"], removed);
+
+			character1.Update(out added, out removed);
+
+			Assert.Equal([], added);
+			Assert.Equal([], removed);
+
+			character2.Position = new Vector2F(45, 45);
+			character3.Position = new Vector2F(55, 55);
+			character1.Update(out added, out removed);
+
+			Assert.Equal(["Character2", "Character3"], added);
+			Assert.Equal([], removed);
+
+			character1.Update(out added, out removed);
+
+			Assert.Equal([], added);
+			Assert.Equal([], removed);
+		}
+
+		private class Character(string name, Vector2F pos)
+		{
+			public string Name { get; } = name;
+			public Map Map { get; set; }
+			public Vector2F Position { get; set; } = pos;
+			public InOutTracker<Character> VisibleCharacters = new();
+
+			public void Update(out List<string> added, out List<string> removed)
+			{
+				added = [];
+				removed = [];
+
+				this.VisibleCharacters.Begin();
+
+				this.Map.GetVisibleCharacters(this, 20, this.VisibleCharacters.UpdateList);
+				this.VisibleCharacters.Update();
+
+				if (!this.VisibleCharacters.Empty)
+				{
+					foreach (var character in this.VisibleCharacters.Added)
+						added.Add(character.Name);
+
+					foreach (var character in this.VisibleCharacters.Removed)
+						removed.Add(character.Name);
+				}
+
+				this.VisibleCharacters.End();
+			}
+		}
+
+		private class Map
+		{
+			public List<Character> Characters = [];
+
+			public void AddCharacter(Character character)
+			{
+				character.Map = this;
+				this.Characters.Add(character);
+			}
+
+			public void RemoveCharacter(Character character)
+			{
+				this.Characters.Remove(character);
+				character.Map = null;
+			}
+
+			public void GetVisibleCharacters(Character character, float radius, IList<Character> result)
+			{
+				foreach (var other in Characters)
+				{
+					if (character == other)
+						continue;
+
+					if (character.Position.InRange(other.Position, radius))
+						result.Add(other);
+				}
+			}
+		}
+	}
+}
---
+++ Yggdrasil/Collections/InOutTracker.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Yggdrasil.Collections
+{
+	/// <summary>
+	/// A helper class for tracking items that are added and removed from
+	/// a list between updates, without allocations after initialization.
+	/// </summary>
+	/// <remarks>
+	/// This class formalizes a pattern of tracking items that are added
+	/// or removed from a list, such as visible actors or trigger areas.
+	/// It's intended to be used exactly as designed and might not work as
+	/// intended if used differently. Please refer to the example for how
+	/// to use it correctly.
+	/// </remarks>
+	/// <example>
+	/// items.Begin();
+	///
+	/// UpdateItems(items.UpdateList); items.Update();
+	///
+	/// if (items.Empty) { items.End(); return; }
+	///
+	/// foreach (var item in items.Added) { // ... }
+	///
+	/// foreach (var item in items.Removed) { // ... }
+	///
+	/// items.End();
+	/// </example>
+	/// <typeparam name="TItem"></typeparam>
+	public class InOutTracker<TItem>
+	{
+		private List<TItem> _prevItems = new List<TItem>();
+		private List<TItem> _curItems = new List<TItem>();
+		private readonly List<TItem> _addedItems = new List<TItem>();
+		private readonly List<TItem> _removedItems = new List<TItem>();
+		private readonly HashSet<TItem> _itemSet = new HashSet<TItem>();
+
+		private bool _updating = false;
+
+		/// <summary>
+		/// Returns true if there's no items in the tracker, indicating
+		/// that no iteration is necessary.
+		/// </summary>
+		public bool Empty
+		{
+			get
+			{
+				if (!_updating)
+					throw new InvalidOperationException("Call Begin() before checking if tracker is empty");
+
+				return _prevItems.Count == 0 && _curItems.Count == 0;
+			}
+		}
+
+		/// <summary>
+		/// Returns the list that should be updated with the current
+		/// items. It's not necessary to clear it.
+		/// </summary>
+		public IList<TItem> UpdateList
+		{
+			get
+			{
+				if (!_updating)
+					throw new InvalidOperationException("Call Begin() before accessing update list");
+
+				return _curItems;
+			}
+		}
+
+		/// <summary>
+		/// Returns the list of items that were added since the last
+		/// update.
+		/// </summary>
+		public IReadOnlyList<TItem> Added
+		{
+			get
+			{
+				if (!_updating)
+					throw new InvalidOperationException("Call Begin() before fetching added items");
+
+				return _addedItems;
+			}
+		}
+
+		/// <summary>
+		/// Returns the list of items that were removed since the last
+		/// update.
+		/// </summary>
+		public IReadOnlyList<TItem> Removed
+		{
+			get
+			{
+				if (!_updating)
+					throw new InvalidOperationException("Call Begin() before fetching removed items");
+
+				return _removedItems;
+			}
+		}
+
+		/// <summary>
+		/// Starts the update, during which the tracker can be updated and
+		/// read.
+		/// </summary>
+		/// <exception cref="InvalidOperationException"></exception>
+		public void Begin()
+		{
+			if (_updating)
+				throw new InvalidOperationException("End previous update using End() before starting new update");
+
+			_updating = true;
+		}
+
+		/// <summary>
+		/// Updates the list of added and removed items.
+		/// </summary>
+		/// <exception cref="InvalidOperationException"></exception>
+		public void Update()
+		{
+			if (!_updating)
+				throw new InvalidOperationException("Call Begin() before updating tracker");
+
+			_itemSet.Clear();
+			foreach (var area in _prevItems)
+				_itemSet.Add(area);
+
+			foreach (var item in _curItems)
+			{
+				if (_itemSet.Contains(item))
+					continue;
+
+				_addedItems.Add(item);
+			}
+
+			_itemSet.Clear();
+			foreach (var area in _curItems)
+				_itemSet.Add(area);
+
+			foreach (var item in _prevItems)
+			{
+				if (_itemSet.Contains(item))
+					continue;
+
+				_removedItems.Add(item);
+			}
+		}
+
+		/// <summary>
+		/// Ends the update and prepares for the next one. This should be
+		/// called after fetching the added and removed items.
+		/// </summary>
+		public void End()
+		{
+			if (!_updating)
+				throw new InvalidOperationException("Call Begin() before ending update");
+
+			_updating = false;
+
+			var temp = _prevItems;
+			_prevItems = _curItems;
+			_curItems = temp;
+
+			_addedItems.Clear();
+			_removedItems.Clear();
+			_curItems.Clear();
+			_itemSet.Clear();
+		}
+	}
+}

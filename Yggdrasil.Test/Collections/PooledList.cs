43c3d5e591dd1660693c7dce38275e65160b4e5d
Exec Tails <exec@dragonbyte.de>
2026-04-19 15:44:38 +0200

    Added PooledList collection
    A generic List type that returns instances from an internal pool. Intended for situations where you need a temporary, short-term list to work with.

---
+++ Yggdrasil.Test/Collections/PooledList.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using Xunit;
+using Yggdrasil.Collections;
+
+namespace Yggdrasil.Test.Collections
+{
+	public class PooledListTests
+	{
+		[Fact]
+		public void Rent_ReturnsEmptyList()
+		{
+			var list = PooledList<int>.Rent();
+			Assert.Empty(list);
+			list.Add(1);
+			list.Dispose();
+
+			var list2 = PooledList<int>.Rent();
+			Assert.Empty(list2);
+			list2.Dispose();
+		}
+
+		[Fact]
+		public void Dispose_ShouldNotBreakIfTriggeredTwice()
+		{
+			var list1 = PooledList<int>.Rent();
+			list1.Dispose();
+			list1.Dispose(); // Shouldn't throw and shouldn't double-return
+
+			var list2 = PooledList<int>.Rent();
+			var list3 = PooledList<int>.Rent();
+
+			// We can't objectively count the pool, but we can verify it
+			// doesn't give us list1 back twice concurrently
+			Assert.NotSame(list2, list3);
+
+			list2.Dispose();
+			list3.Dispose();
+		}
+
+		[Fact]
+		public void Dispose_ListOverMaxCapacity_Discarded()
+		{
+			var list = PooledList<int>.Rent();
+			list.Capacity = 2048; // Artificial inflation over 1024
+			list.Add(1);
+
+			list.Dispose();
+
+			// Because it was discarded, renting should provide a new list
+			var list2 = PooledList<int>.Rent();
+			Assert.NotSame(list, list2);
+			list2.Dispose();
+		}
+
+		[Fact]
+		public void Dispose_PoolFull_ExtraDiscarded()
+		{
+			var itemsToRent = 5005;
+			var lists = new List<PooledList<int>>(itemsToRent);
+
+			// Rent and return 5005 lists, which is over the pool max size
+			for (var i = 0; i < itemsToRent; ++i)
+				lists.Add(PooledList<int>.Rent());
+
+			for (var i = 0; i < itemsToRent; ++i)
+				lists[i].Dispose();
+
+			// We shouldn't crash, the extra 5 just get thrown away
+			var finalList = PooledList<int>.Rent();
+			Assert.NotNull(finalList);
+			finalList.Dispose();
+		}
+	}
+}
---
+++ Yggdrasil/Collections/PooledList.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Yggdrasil.Collections
+{
+	/// <summary>
+	/// A List implementation that takes its lists from a shared pool to
+	/// minimize allocations.
+	/// </summary>
+	/// <remarks>
+	/// The class functions the exact same as a normal List, except that
+	/// its instances are taken from a shared pool. After use, the list
+	/// needs to be disposed, so it can be returned to the pool. It's
+	/// intended for temporary lists used in hot paths and should not be
+	/// used for long-term storage.
+	/// </remarks>
+	/// <typeparam name="T"></typeparam>
+	public class PooledList<T> : List<T>, IDisposable
+	{
+		private static readonly ConcurrentBag<PooledList<T>> Pool = new ConcurrentBag<PooledList<T>>();
+		private const int MaxPoolSize = 5000;
+		private const int MaxCapacity = 1024;
+		private static int PoolCount;
+
+		private bool _disposed;
+
+		/// <summary>
+		/// Prevent external instantiation.
+		/// </summary>
+		private PooledList() : base()
+		{
+		}
+
+		/// <summary>
+		/// Returns a list from the pool.
+		/// </summary>
+		/// <remarks>
+		/// It's highly recommended to use this method in a using
+		/// statement, so the list will be returned to the pool
+		/// semi-automatically.
+		/// </remarks>
+		/// <returns></returns>
+		public static PooledList<T> Rent()
+		{
+			if (Pool.TryTake(out var list))
+				Interlocked.Decrement(ref PoolCount);
+			else
+				list = new PooledList<T>();
+
+			list._disposed = false;
+			return list;
+		}
+
+		/// <summary>
+		/// Disposes the list and returns it to the pool. After calling
+		/// this method, the list should not be used anymore.
+		/// </summary>
+		public void Dispose()
+		{
+			if (_disposed)
+				return;
+
+			_disposed = true;
+
+			if (this.Capacity >= MaxCapacity)
+			{
+				this.Clear();
+				return;
+			}
+
+			this.Clear();
+
+			// Return the list to the pool up to the max pool size
+			if (Interlocked.Increment(ref PoolCount) <= MaxPoolSize)
+				Pool.Add(this);
+			else
+				Interlocked.Decrement(ref PoolCount);
+		}
+	}
+}
